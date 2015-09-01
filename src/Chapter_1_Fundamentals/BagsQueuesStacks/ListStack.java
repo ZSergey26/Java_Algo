93a68ab4208fd287c272d27e0ac9952a52f69f45
Sergei Zarochentsev <zsergey26@gmail.com>
2015-09-01 15:04:56 +0300

    List and Queue

---
+++ src/Chapter_1_Fundamentals/BagsQueuesStacks/ListStack.java
@@ -0,0 +1,94 @@
+package Chapter_1_Fundamentals.BagsQueuesStacks;
+
+import java.util.Iterator;
+
+/**
+ * Created by Sergei Zarochentsev on 31.08.2015.
+ */
+public class ListStack<Item> implements Iterable<Item> {
+
+    @Override
+    public Iterator<Item> iterator() {
+        return new  ListStackIterator();
+    }
+
+    private Node first;
+    private class Node
+    {
+        Item item;
+        Node next;
+    }
+
+    private int N;
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void push(Item item)
+    {
+        Node oldfirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldfirst;
+        N++;
+    }
+
+    public Item pop()
+    {
+        Item item = first.item;
+        first = first.next;
+        N--;
+        return item;
+    }
+
+    private class ListStackIterator implements Iterator<Item> {
+        private Node iter = first;
+        @Override
+        public boolean hasNext() {
+            return iter != null;
+        }
+
+        @Override
+        public Item next() {
+            Item item = iter.item;
+            iter = iter.next;
+            return item;
+        }
+    }
+
+    public void revert()
+    {
+
+    }
+
+
+    public static void main(String[] args) {
+        ListStack<String> listStack = new ListStack<>();
+
+        listStack.push("to");
+        listStack.push("be");
+        listStack.push("or");
+        listStack.push("not");
+
+        System.out.print("List: ");
+        for (String s : listStack) {
+            System.out.print(s + " <- ");
+        }
+
+        for (String s : listStack) {
+            listStack.push(listStack.pop());
+        }
+
+        System.out.println();
+        System.out.print("List: ");
+        for (String s : listStack) {
+            System.out.print(s + " <- ");
+        }
+    }
+
+}
---
+++ src/Chapter_1_Fundamentals/BagsQueuesStacks/MyQueue.java
@@ -0,0 +1,99 @@
+package Chapter_1_Fundamentals.BagsQueuesStacks;
+
+import java.util.Iterator;
+
+/**
+ * Created by Sergei Zarochentsev on 01.09.2015.
+ */
+public class MyQueue<Item>  implements Iterable<Item>  {
+
+    @Override
+    public Iterator<Item> iterator() {
+        return new  QueueIterator();
+    }
+
+    private Node first;
+    private Node last;
+    private int N;
+
+
+    private class Node
+    {
+        Item item;
+        Node next;
+    }
+
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void enqueue(Item item)
+    {
+        Node oldlast = last;
+        last = new Node();
+        last.item = item;
+        last.next = null;
+        if(isEmpty())
+            first = last;
+        else
+            oldlast.next = last;
+
+        N++;
+    }
+
+    public Item dequeue()
+    {
+        Item item = first.item;
+        first = first.next;
+
+        if(isEmpty())
+            last = null;
+
+        N--;
+        return item;
+    }
+
+    private class QueueIterator implements Iterator<Item> {
+        private Node iter = first;
+        @Override
+        public boolean hasNext() {
+            return iter != null;
+        }
+
+        @Override
+        public Item next() {
+            Item item = iter.item;
+            iter = iter.next;
+            return item;
+        }
+    }
+
+    public static void main(String[] args) {
+        MyQueue<String> queue = new MyQueue<>();
+
+        queue.enqueue("One");
+        queue.enqueue("Two");
+        queue.enqueue("Three");
+
+        System.out.println();
+        for (String s : queue) {
+            System.out.print(s + " ");
+        }
+
+        for (int i = 0; i < queue.size(); i++) {
+            queue.enqueue(queue.dequeue());
+        }
+
+
+
+        System.out.println();
+        for (String s : queue) {
+            System.out.print(s + " ");
+        }
+    }
+}
